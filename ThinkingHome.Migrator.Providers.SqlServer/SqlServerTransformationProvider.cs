e43b958bbffeb05cf76023e400c46b7f24c6446c
geoff_short_bfdx <geoff.short@biofiredx.com>
2023-07-03 14:21:18 -0600

    change to .NET 6 and microsoft.data.sqlclient

--- ThinkingHome.Migrator.Providers.SqlServer/SqlServerTransformationProvider.cs
+++ ThinkingHome.Migrator.Providers.SqlServer/SqlServerTransformationProvider.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Text;
 using Microsoft.Extensions.Logging;
 using ThinkingHome.Migrator.Framework;
